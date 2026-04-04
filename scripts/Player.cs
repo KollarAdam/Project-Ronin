65aa90d7ac25d58cd1770ef24728df536396d15d
Kollár Ádám <kollaradam68@gmail.com>
2026-04-04 15:03:24 +0200

    more setup

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -5,11 +5,16 @@ public partial class Player : CharacterBody2D
 {
 	[ExportGroup("Movement")]
 	[Export] private float _baseMoveSpeed = 10f;
-	[Export] private float _baseJumpHeight = 5f;
+	[Export] private float _maxMoveSpeed = 20f;
+	[Export] private float _acceleration = 1000f;
+	[Export] private float _airAcceleration = 50f;
+	[Export] private float _airFriction = 1000f;
+	[Export] private float _friction = 2000f;
+	[Export] private float _upGravity = 500f;
+	[Export] private float _downGravity = 600f;
+	[Export] private float _jumpStrength = 200f;
 	[Export] private int _extraJumps = 1;
-
 	private Vector2 _moveDirection = Vector2.Zero;
-
     public override void _UnhandledKeyInput(InputEvent @event)
     {
         var inputDirection = Input.GetAxis("move_left", "move_right");
@@ -18,12 +23,17 @@ public partial class Player : CharacterBody2D
 
 	private void HandleMovement()
 	{
-		if(_moveDirection == Vector2.Zero)
-		{
-			Velocity = Vector2.Zero;
-			return;
-		}
 		Velocity = _moveDirection * _baseMoveSpeed;
+		
+	}
+	private void accelerateHorizontally(float delta, float horizontalDirection)
+	{
+		Vector2 velocity = Velocity;
+		var accelerationAmount = _acceleration;
+		if (!IsOnFloor())
+		{
+			accelerationAmount = _airAcceleration;
+		}
 	}
 
     public override void _PhysicsProcess(double delta)
