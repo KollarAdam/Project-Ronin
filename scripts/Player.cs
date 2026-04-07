85eff45af5538b873c94bebfe4d8777bb6a31c65
Kollár Ádám <kollaradam68@gmail.com>
2026-04-07 13:16:07 +0200

    Updated movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -6,40 +6,91 @@ public partial class Player : CharacterBody2D
 	[ExportGroup("Movement")]
 	[Export] private float _baseMoveSpeed = 100f;
 	[Export] private float _maxMoveSpeed = 200f;
-	[Export] private float _acceleration = 1000f;
-	[Export] private float _airAcceleration = 50f;
-	[Export] private float _airFriction = 1000f;
+	[Export] private float _acceleration = 2000f;
+	[Export] private float _airAcceleration = 1000f;
+	[Export] private float _airFriction = 700f;
 	[Export] private float _friction = 2000f;
 	[Export] private float _upGravity = 500f;
-	[Export] private float _downGravity = 600f;
+	[Export] private float _downGravity = 700f;
 	[Export] private float _jumpStrength = 200f;
 	[Export] private int _extraJumps = 1;
-	private Vector2 _moveDirection = Vector2.Zero;
-    public override void _UnhandledKeyInput(InputEvent @event)
-    {
-        var inputDirection = Input.GetAxis("move_left", "move_right");
-		_moveDirection = new Vector2(inputDirection, 0).Normalized();
-    }
-
-	private void HandleMovement()
-	{
-		Velocity = _moveDirection * _baseMoveSpeed;
-		
-	}
-	private void accelerateHorizontally(float delta, float horizontalDirection)
-	{
-		Vector2 velocity = Velocity;
-		var accelerationAmount = _acceleration;
-		if (!IsOnFloor())
-		{
-			accelerationAmount = _airAcceleration;
-		}
-	}
+	
+	private int _remainingJumps = 1;
 
     public override void _PhysicsProcess(double delta)
     {
-		HandleMovement();
+		var velocity = Velocity;
+		var inputDirection = Input.GetAxis("move_left", "move_right");
+		var inputJump = Input.IsActionJustPressed("jump");
+
+		velocity.Y = _ApplyGravity(delta);
+		_ResetJumps();
+
+		if (inputJump)
+		{
+			velocity.Y = _ApplyJump();
+		}	
+
+		if (inputDirection == 0)
+		{
+			velocity.X = _ApplyFriction(delta);
+		}
+		else
+		{
+			velocity.X = _AccelerateHorizontally(inputDirection, delta);
+		}
+
+		Velocity = velocity;
+
 		MoveAndSlide();
     }
 
+	private float _AccelerateHorizontally(float horDir, double delta)
+	{
+		var velocity = Velocity;
+		var accelerationAmount = _acceleration;
+		if(!IsOnFloor()) accelerationAmount = _airAcceleration;
+		velocity.X = Mathf.MoveToward(velocity.X, _maxMoveSpeed * horDir, (float)(accelerationAmount * delta * Math.Abs(horDir)));
+		return velocity.X;
+	}
+
+	private float _ApplyFriction(double delta)
+	{
+		var velocity = Velocity;
+		var frictionAmount = _friction;
+		if(!IsOnFloor()) frictionAmount = _airFriction;
+		velocity.X = Mathf.MoveToward(velocity.X, 0f, (float)(frictionAmount * delta));
+		return velocity.X;
+	}
+
+	private float _ApplyGravity(double delta)
+	{
+		var velocity = Velocity;
+		if (!IsOnFloor())
+		{
+			if (velocity.Y <= 0) velocity.Y += (float)(_upGravity * delta);
+			else velocity.Y += (float)(_downGravity * delta);
+		}
+		return velocity.Y;
+	}
+
+	private float _ApplyJump()
+	{
+		var velocity = Velocity;
+		if (IsOnFloor())
+		{
+			velocity.Y = -_jumpStrength;
+		}
+		if (!IsOnFloor() && _remainingJumps != 0)
+		{
+			velocity.Y = -_jumpStrength;
+			_remainingJumps--;
+		}
+		return velocity.Y;
+	}
+
+	private void _ResetJumps()
+	{
+		if(IsOnFloor()) _remainingJumps = _extraJumps;
+	}
 }
