1fda6755cac6794bae1f3773201fb163cd15faa0
Kollár Ádám <kollaradam68@gmail.com>
2026-07-01 00:49:38 +0200

    Bg added, enemy won't run off a cliff while chasing

--- scripts/States/Enemy/EnemyChase.cs
+++ scripts/States/Enemy/EnemyChase.cs
@@ -5,7 +5,6 @@ public partial class EnemyChase : EnemyState
 {
     Player player;
     private float _moveDir;
-    bool posMoveDir;
     public override void _Ready()
     {
         base._Ready();
@@ -13,11 +12,10 @@ public partial class EnemyChase : EnemyState
     }
     public override void PhysicsProcess(double delta)
     {
+        var velocity = enemy.Velocity;
         _moveDir = Math.Clamp(player.GlobalPosition.X - enemy.GlobalPosition.X, -1, 1);
-        posMoveDir = _moveDir > 0;
         if (enemy.siteLine.HasOverlappingAreas())
         {
-            var velocity = enemy.Velocity;
             velocity.X = enemy.movement.AccelerateHorizontally(_moveDir, velocity.X, delta, enemy.IsOnFloor());
             enemy.Velocity = velocity;
         }
@@ -25,6 +23,10 @@ public partial class EnemyChase : EnemyState
         {
             enemy.Velocity = Vector2.Zero;
             StateChanged?.Invoke(this, "ENEMYIDLE");
+        }
+        if (!enemy.mapEdge.IsColliding())
+        {
+            enemy.Velocity = Vector2.Zero;
         }
     }
 
